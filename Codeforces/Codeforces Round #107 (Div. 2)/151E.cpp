ea74e8ecd195a79438a57528f6d147fd3662ae32
ZengMing1997 <173258027@qq.com>
2019-04-01 13:31:32 +0800

    Codeforces

---
+++ Codeforces/Codeforces Round #107 (Div. 2)/151E.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 150000;
+int n , m , q;
+int a[maxn];
+int main()
+{
+    scanf("%d%d%d",&n,&m,&q);
+    for(int i = 1;  i <= n ; i++) scanf("%d",&a[i]);
+}
