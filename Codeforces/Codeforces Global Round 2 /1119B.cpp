c87f2322e3bf770eb4011704ad0759e323c9d118
ZengMing1997 <173258027@qq.com>
2019-04-09 20:47:40 +0800

    1

--- Codeforces/Codeforces Global Round 2 /1119B.cpp
+++ Codeforces/Codeforces Global Round 2 /1119B.cpp
@@ -5,27 +5,24 @@ int a[maxn] , sum[maxn];
 int n , h;
 int check(int pos)
 {
-    //puts("#########################");
     vector<int>V;
     for(int i = 0 ; i < pos ; i++) V.push_back(a[i]);
     long long h1 = 0;
     sort(V.begin() , V.end(),[](int a,int b){return a > b;});
     for(int i = 0 ; i < V.size() ; i = i + 2) h1 += V[i];
     return h1 <= h;
-
 }
 int main()
 {
     
     scanf("%d%d",&n,&h);
     for(int i = 0 ; i < n ; i++) scanf("%d",&a[i]);
-    int l = 0 , r = n , ans = n;
-    while(l<=r)
+    int l = 0, r = n + 1;
+    while(l < r)
     {
         int m = (l+r)/2;
-        if(check(m)) l = m + 1 , ans = m;
-        else r = m-1;
+        if(check(m)) l = m + 1;
+        else r = m;
     }
-    if(!check(ans)) ans--;
-    cout<<ans<<endl;
+    cout<< l - 1 <<endl;
 }
