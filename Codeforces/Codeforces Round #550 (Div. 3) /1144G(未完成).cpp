c5313394f888932cda8971644427cf97687145a2
ZengMing1997 <173258027@qq.com>
2019-04-18 14:20:01 +0800

    1

---
+++ Codeforces/Codeforces Round #550 (Div. 3) /1144G(未完成).cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 2e5+10;
+int a[maxn];
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i = 0 ; i < n ;i++) scanf("%d",&a[i]);
+    
+}
