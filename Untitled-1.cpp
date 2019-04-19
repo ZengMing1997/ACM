d39b08e4c0500e9d0bfc049488efd13c7dbe3eed
ZengMing1997 <173258027@qq.com>
2019-04-19 11:02:18 +0800

    1

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,93 +1,54 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e5+8;
-#define int long long
-int a[maxn] , b[maxn];
-signed main()
+const int Roulette_size = 12;
+const int Box_size = 6;
+const int Goods_number = 3000;
+const int Roulette_speed = 3;
+const int Box_speed = 1;
+int Roulette_cur_pos , Box_cur_pos,curtime;
+struct Goods
 {
-	int t , n , k ;
-	scanf("%lld" , &t);
-	while(t--)
+	int OrderID , GoodsID;
+	int frame_pos,Box_pos , Roulette_pos;
+	Goods (int OrderID , int GoodsID , int frame_pos,int Box_pos,int Roulette_pos) : 
+	OrderID(OrderID) , GoodsID(GoodsID) ,frame_pos(frame_pos),Box_pos(Box_pos),Roulette_pos(Roulette_pos) {}
+};
+bool Roulette_is_used[Roulette_size];
+vector<pair<int,int> >Box_detil[Roulette_size][Box_size];
+vector<Goods>all_Goods;
+map<int,queue<Goods> >Need_Time;
+bool check(Goods a)
+{
+	while(Roulette_is_used[Roulette_cur_pos] == true)
 	{
-		int x;
-		int p = 1,q = 1 , ans = 0;
-		scanf("%lld %lld" , &n , &k);
-		for(int i = 1 ; i <= n ; i++)
+		Roulette_cur_pos = (curtime + 1) % Roulette_size;
+		Box_cur_pos = (Box_cur_pos+1) % (Box_size * Roulette_speed);
+		curtime = curtime + 1;
+		while(!Need_Time[curtime].empty())
 		{
-			scanf("%lld" ,&x);
-			if(x < 0) 
-			{
-				a[p] = -x;
-				p++;
-			}
-			if(x > 0) 
-			{
-				b[q] = x;
-				q++;
-			}
+			Goods temp = Need_Time[curtime].front();
+			Need_Time[curtime].pop();
+			Roulette_is_used[temp.Roulette_pos]= 0;
+			Box_detil[temp.frame_pos][temp.Box_pos].push_back(make_pair(temp.GoodsID,temp.OrderID));
 		}
-	//	printf("p = %d q = %d\n" , p , q);
-		sort(a+1 , a+p);
-		sort(b+1 , b+q);
-		if(a[p-1] <= b[q-1] && (a[p-1]!=0 && b[q-1]!= 0))
-		{
-			int x = p-1,y = q-1;
-			while(x>0)
-			{
-				ans += a[x];
-				x -= k;
-			}
-			while(y>0)
-			{
-				y -= k;
-				ans += b[y];
-			}
-			ans = ans *2 + b[q-1];
-		}
-		else if(a[p-1] > b[q-1] && ( a[p-1]!=0 && b[q-1]!= 0))
-		{
-			int x = p-1,y = q-1;
-			while(x > 0)
-			{
-				x -= k;
-				ans += a[x];
-			}
-			while(y>0)
-			{
-				ans += b[y];
-				y -= k; 
-			}
-			ans = ans *2 + a[p-1];
-		}
-		else if(a[p-1]==0 && b[q-1]!= 0)
-		{
-			int x = p-1,y = q-1;
-			while(y>0)
-			{
-				ans += b[y];
-				y -= k;
-			}
-			ans = ans *2;
-		}
-		else if(a[p-1]!=0 && b[q-1]== 0)
-		{
-			int x = p-1,y = q-1;
-			while(x>0)
-			{
-				ans += a[x];
-				x -= k;
-			}
-			ans = ans *2;
-		}
-		printf("%lld\n" , ans);
-	 } 
-	return 0;
+	}
+	Roulette_is_used[Roulette_cur_pos] = 1;
+	a.Roulette_pos = Roulette_cur_pos;
+	int needtime = a.frame_pos + Box_cur_pos;
+	if(needtime / Roulette_speed + (needtime % Roulette_speed == 0) == a.Box_pos)
+	{
+		Need_Time[curtime+a.frame_pos].push(a);
+	}
+	else 
+	{
+
+	}
 }
-/*
-2
-5 3
--1 -2 3 -4 -5
-6 3
-1 0 -2 -1 1 2
-*/
+int main()
+{
+	Roulette_is_used[0] = 1;
+	for(auto i : all_Goods)
+	{
+		if(check(i));
+	}
+}
